3202b0efcc134c038902ceb54e906e87b0a10f36
Emzzy241 <emzzyoluwole@gmail.com>
2026-03-12 15:10:45 +0100

    add rate limiter middleware for sending verification code

---
+++ middlewares/rateLimiter.js
@@ -0,0 +1,12 @@
+const rateLimit = require("express-rate-limit");
+
+exports.verificationCodeLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // max 10 requests per IP
+    message: {
+        success: false,
+        message: "Too many verification requests from this IP. Try again later."
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
