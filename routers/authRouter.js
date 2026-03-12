e48609010a8231198cb0499f4c903e1d0c5da230
Emzzy241 <emzzyoluwole@gmail.com>
2026-03-12 15:14:15 +0100

    security: update auth router with rate-limiting logic

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const authController = require('../controllers/authController');
 const { identifier } = require('../middlewares/identification');
+const { verificationCodeLimiter } = require("../middlewares/rateLimiter");
 const router = express.Router()
 
 router.get('/greeting', authController.greeting);
@@ -9,7 +10,7 @@ router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
 router.post('/signout', authController.signout);
 
-router.patch('/send-verification-code', identifier, authController.sendVerificationCode);
+router.patch('/send-verification-code', verificationCodeLimiter, authController.sendVerificationCode);
 router.patch('/verify-verification-code', identifier, authController.verifyVerificationCode);
 router.patch('/change-password', identifier, authController.changePassword);
 router.patch('/send-forgot-password-code', authController.sendForgotPasswordCode);
