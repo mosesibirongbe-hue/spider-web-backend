3f856d7c142bafd5f7cbdcca263ad84a902bd5f9
Emzzy241 <emzzyoluwole@gmail.com>
2026-03-13 10:08:35 +0100

    update auth router: import rateLimiter logic for forgotPassword + add correct middleware

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const authController = require('../controllers/authController');
 const { identifier } = require('../middlewares/identification');
-const { verificationCodeLimiter } = require("../middlewares/rateLimiter");
+const { verificationCodeLimiter,forgotPasswordCodeLimiter } = require("../middlewares/rateLimiter");
 const router = express.Router()
 
 router.get('/greeting', authController.greeting);
@@ -13,7 +13,7 @@ router.post('/signout', authController.signout);
 router.patch('/send-verification-code', verificationCodeLimiter, authController.sendVerificationCode);
 router.patch('/verify-verification-code', authController.verifyVerificationCode);
 router.patch('/change-password', identifier, authController.changePassword);
-router.patch('/send-forgot-password-code', authController.sendForgotPasswordCode);
+router.patch('/send-forgot-password-code', forgotPasswordCodeLimiter, authController.sendForgotPasswordCode);
 router.patch('/verify-forgot-password-code', authController.verifyForgotPasswordCode);
 
 module.exports = router
